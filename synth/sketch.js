88eb17705c8181d35dddbeb2be37c673d3cf9df2
lucidbeaming <zenphoto@gmail.com>
2017-02-23 19:58:30 -0800

    oscillator types

--- synth/sketch.js
+++ synth/sketch.js
@@ -1,10 +1,11 @@
-var playing1, playing2;
+var playing1, playing2, osc1obj, osc2obj;
+var osctypes = ['sine', 'triangle', 'sawtooth', 'square']
 
 function setup() {
     createCanvas(720, 256);
 
-    osc1 = new p5.TriOsc(); // set frequency and type
-    osc2 = new p5.SqrOsc();
+    osc1 = new p5.SinOsc(); // set frequency and type
+    osc2 = new p5.SinOsc();
     osc1.amp(.5); // set volume to osc1
     osc2.amp(.5); // set volume to osc2
 
@@ -13,9 +14,37 @@ function setup() {
     fft1.setInput(osc1); // send osc1 to fft1
     fft2.setInput(osc2); // send osc2 to fft2
     amplitude = new p5.Amplitude();
+
+    osc1obj = select('#osc1');
+    osc2obj = select('#osc2');
+
+    radio1 = createRadio('osc1controls');
+    radio1.option("sin", 0);
+    radio1.option("tri", 1);
+    radio1.option("saw", 2);
+    radio1.option("sqr", 3);
+    radio1.style('width', '100px');
+    textAlign(CENTER);
+    fill(0, 180, 0);
+
+    radio2 = createRadio('osc2controls');
+    radio2.option("sin", 0);
+    radio2.option("tri", 1);
+    radio2.option("saw", 2);
+    radio2.option("sqr", 3);
+    radio2.style('width', '100px');
+    textAlign(CENTER);
+    fill(0, 180, 0);
+
+    osc1obj.child(radio1);
+    osc2obj.child(radio2);
+
+    radio1.value('0');
+    radio2.value('0');
 }
 
 function draw() {
+
     background(255);
 
     var waveform1 = fft1.waveform(); // analyze the 1st waveform
@@ -71,4 +100,13 @@ function keyPressed() {
         }
         return false; // prevent default
     }
+}
+
+function mouseClicked() {
+    var r1 = radio1.value();
+    var r2 = radio2.value();
+
+    osc1.setType(osctypes[r1]);
+    osc2.setType(osctypes[r2]);
+    console.log('osc1 wave = ' + osctypes[r1] + ', osc2 wave = ' + osctypes[r2]);
 }
